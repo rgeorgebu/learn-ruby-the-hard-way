9cce705115e442c797aa57dc6524d236bfbf6269
rgeorgebu <rgeorgebu@gmail.com>
2022-07-23 11:27:59 -0500

    Exercise 5

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'R. George Burkhardt'
+my_age = 23 # Not a lie in 2022
+my_height = 69 # Nice
+my_weight = 175 # I'm trying for 155
+my_eyes = 'Hazel' # No they are not brown - I promise
+my_teeth = 'Yellow-ish' # Coffee is too delicious
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} inches tall"
+puts "He's #{my_weight} pounds heavy"
+puts "Actually that's not too heavy"
+puts "He's got #{my_eyes} eyes and #{my_hair} hair"
+puts "His teeth are usually #{my_teeth} depending on the coffee"
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
