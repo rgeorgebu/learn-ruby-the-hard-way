b13c4afd0acaef50c1ba07229e8d0b6dd9083e63
rgeorgebu <rgeorgebu@gmail.com>
2022-08-18 20:17:46 -0500

    Testing module exports/attributes

---
+++ mystuff.rb
@@ -0,0 +1,11 @@
+module MyStuff
+    def MyStuff.apple
+        puts "I AM APPLES!"
+    end
+
+    def orange
+        puts "ORANGE RHYMES WITH ORANGE"
+    end
+
+    TANGERINE = 'Living reflection of a dream'
+end
---
+++ test_mystuff.rb
@@ -0,0 +1,10 @@
+require './mystuff.rb'
+
+MyStuff.apple
+MyStuff::apple
+
+# MyStuff.orange - throws error
+# MyStuff::orange - throws error
+
+# MyStuff.TANGERINE - throws error
+puts MyStuff::TANGERINE
