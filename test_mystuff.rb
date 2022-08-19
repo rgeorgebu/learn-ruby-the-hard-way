87bab812aef508037e696993dddcb570a9cfc453
rgeorgebu <rgeorgebu@gmail.com>
2022-08-18 20:45:12 -0500

    Trying some module vs class things

--- test_mystuff.rb
+++ test_mystuff.rb
@@ -8,3 +8,57 @@ MyStuff::apple
 
 # MyStuff.TANGERINE - throws error
 puts MyStuff::TANGERINE
+
+
+class MoreStuff
+    def MoreStuff.apple
+        puts "apples"
+    end
+
+    def orange
+        puts "oranges"
+    end
+
+    TANGERINE = 'getting tan'
+    @MANGO = 'smoothie'
+    @@ABC = 'DEF'
+end
+
+MoreStuff.apple
+MoreStuff::apple
+
+# MoreStuff.orange - errors
+# MoreStuff::orange - errors
+
+# puts MoreStuff.TANGERINE - errors
+puts MoreStuff::TANGERINE
+
+# puts MoreStuff.MANGO - errors
+# puts MoreStuff::MANGO - errors
+
+# puts MoreStuff.ABC - errors
+# puts MoreStuff::ABC - errors
+
+s = MoreStuff::new
+# s.apple - errors
+# s::apple - errors
+s.orange
+s::orange
+# s.TANGERINE - errors
+# s::TANGERINE - errors
+# s.MANGO - errors
+# s::MANGO - errors 
+# s.ABC - errors
+# s::ABC - errors
+
+ss = MoreStuff.new
+# s.apple - errors
+# s::apple - errors
+s.orange
+s::orange
+# s.TANGERINE - errors
+# s::TANGERINE - errors
+# s.MANGO - errors
+# s::MANGO - errors
+# s.ABC - errors
+# s::ABC - errors
